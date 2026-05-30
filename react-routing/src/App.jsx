ebca607e2214aacc8e73af779fe47c1fd0e856d3
Uzair-Debugger <smuzair14cse@gmail.com>
2026-05-30 16:41:06 +0500

    Task 1 Completed

--- react-routing/src/App.jsx
+++ react-routing/src/App.jsx
@@ -1,13 +1,20 @@
-
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 function App() {
-
   return (
-    <>
-      <main>
-
-      </main>
-    </>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/contact">Contact</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
